88406aee339d1f1b480ef1d4f322d5ddca6a96a6
Amin <mavericane@gmail.com>
2023-08-23 05:57:20 +0330

    Problem-3 solution Added

---
+++ solutions/problem-3.py
@@ -0,0 +1,21 @@
+import math
+
+def prime_factor(number):
+    if number < 2:
+        print("Your entered number is smaller than first prime number!")
+    else:
+        for i in range(2,int(math.sqrt(number))+1):
+            if number % i == 0:
+                return i
+        return number
+    
+number = 600851475143
+
+while True:
+    prime = prime_factor(number)
+
+    if prime < number:
+        number //= prime
+    else:
+        print(number)
+        break
