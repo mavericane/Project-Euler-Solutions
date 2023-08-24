696ef63bb3def13f42eec500f0bdd1a502f5e956
Amin <aminkhatoonabadi@gmail.com>
2023-08-24 05:32:42 +0330

    Problem-6 solution Added

---
+++ solutions/problem-6.py
@@ -0,0 +1,11 @@
+sum_of_squares = 0
+sum = 0
+for number in range(100+1):
+    sum += number
+    sum_of_squares += number**2
+
+square_of_sum = sum**2
+
+result = square_of_sum - sum_of_squares
+
+print(result)
