4e22a2f9b96be62ced1e14e855d7112fa79498af
Amin <aminkhatoonabadi@gmail.com>
2023-08-26 02:29:15 +0330

    Problem-7 solution Added

---
+++ solutions/problem-7.py
@@ -0,0 +1,25 @@
+import math
+
+def isprime(number):
+    if number <= 1:
+        return False
+    elif number <= 3:
+        return True
+    else:
+        for i in range(2,int(math.sqrt(number)+1)):
+            if number % i == 0:
+                return False
+        return True
+
+counter = 0
+number = 0
+while True:
+    if isprime(number):
+        counter += 1
+        if counter == 10001:
+            break
+    
+    number += 1
+
+    
+print(number)
